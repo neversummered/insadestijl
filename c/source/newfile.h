7b6b2bbe47e71d9e45e15db488ff5ba9027814c2
pehladik@gmail.com <pehladik@gmail.com@7d58bca3-0b7d-ecd7-d460-11c0825f10c2>
2012-01-16 08:37:19 +0000

    Import de l'implementation en c

---
+++ c/source/newfile.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   newfile.h
+ * Author: pehladik
+ *
+ * Created on 12 janvier 2012, 11:37
+ */
+
+#ifndef NEWFILE_H
+#define	NEWFILE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* NEWFILE_H */
+
