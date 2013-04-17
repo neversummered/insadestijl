5f797829b951df00b4f4521dc33f6b5504c91000
pehladik@gmail.com <pehladik@gmail.com@7d58bca3-0b7d-ecd7-d460-11c0825f10c2>
2013-04-17 15:42:21 +0000

    mise a jour eval duree

--- lib/tests/eval_duree/main.c
+++ lib/tests/eval_duree/main.c
@@ -1,51 +1,52 @@
 /* 
- * File:   main.cpp
- * Author: Piro
+ * File:    main.cpp
+ * Author:  Piro
  *
- * Created on 6 décembre 2011, 07:47
+ * Created on 6 décembre 2011, 07: 47
  */
 
 
 #include <stdlib.h> 
 #include "../headers/dincludes.h"
 
+#define NBE 100
 
-double moyenne(long long mesures[100]);
-double ecart_type(long long mesures[100], double moyenne);
-double min(long long mesures[100]);
-double max(long long mesures[100]);
+double moyenne(long long mesures[NBE]);
+double ecart_type(long long mesures[NBE], double moyenne);
+double min(long long mesures[NBE]);
+double max(long long mesures[NBE]);
 
-double moyenne(long long mesures[100]) {
+double moyenne(long long mesures[NBE]) {
     int i;
     long long somme = 0;
-    for (i = 0; i < 100; i++) {
+    for (i = 0; i < NBE; i++) {
         somme += mesures[i];
     }
-    return (double) somme / 100.0;
+    return (double) somme / (double) NBE;
 }
 
-double ecart_type(long long mesures[100], double moyenne) {
+double ecart_type(long long mesures[NBE], double moyenne) {
     long long somme = 0;
     int i;
-    for (i=0; i<100; i++){
+    for (i=0; i<NBE; i++){
         somme += pow((double)mesures[i] - moyenne,2.0);
     }
-    return (double) somme / 100;
+    return (double) somme / (double) NBE;
 }
 
-double min(long long mesures[100]) {
-	long long m = ~(0x0);
+double min(long long mesures[NBE]) {
+	long long m = 1000000000000000000.0;
     int i;
-	for (i=0; i<100; i++){
+	for (i=0; i< NBE; i++){
         if (mesures[i] < m) m = mesures[i]; 
     }
     return (double) m;
 }
 
-double max(long long mesures[100]) {
+double max(long long mesures[NBE]) {
 	long long m = 0;
     int i;
-	for (i=0; i<100; i++){
+	for (i=0; i<NBE; i++){
         if (mesures[i] > m) m = mesures[i]; 
     }
     return (double) m;
@@ -55,7 +56,7 @@ int main(int argc, char** argv) {
 	
     long long t1;
     long long t2;
-    long long mesures[100];
+    long long mesures[NBE];
     double moy, e, mi, ma;
 	
 	int real = 0;
@@ -67,10 +68,12 @@ int main(int argc, char** argv) {
     DArena* arena = NULL;
 	
 	if (!real) { //Utilisation d'une image fixe
+		printf("Chargement image\n");
 		IplImage* imgSrc = cvLoadImage("Photo1c.jpg", CV_LOAD_IMAGE_COLOR);
 		d_image_set_ipl(img, imgSrc);
 		cvReleaseImage(&imgSrc);
 	} else {
+		printf("ouverture camera\n");
 		camera = d_new_camera();
 		d_camera_open(camera);
 	}
@@ -81,7 +84,8 @@ int main(int argc, char** argv) {
 	 d_camera_get_frame
  	*****************************/
 	if (real) {
-		for (i = 0; i < 100; i++) {
+		printf("Evalution du temps pour d_camera_get_frame\n");
+		for (i = 0; i < NBE; i++) {
 			t1 = d_tools_get_time_in_millisec();
 			d_camera_get_frame(camera, img);
 			t2 = d_tools_get_time_in_millisec();
@@ -91,14 +95,15 @@ int main(int argc, char** argv) {
 		e = ecart_type(mesures, moy);
 		mi = min(mesures);
 		ma = max(mesures);
-		printf("Temps moyen d_camera_get_frame:%f (ecart type:%f, min:%f max%f)\n", moy, e, mi, ma);
+		printf(">  Temps moyen: %f ecart type: %f min: %f max: %f\n", moy, e, mi, ma);
 	}    
 	
 	/*****************************
 	 Temps pour le calcul de la position du robot avec arene
 	 d_image_compute_arena_position
 	 *****************************/
-    for (i = 0; i < 100; i++) {
+	printf("Evalution du temps pour d_image_compute_arena_position\n");
+    for (i = 0; i < NBE; i++) {
         if (real) d_camera_get_frame(camera, img);
         t1 = d_tools_get_time_in_millisec();
         d_image_compute_arena_position(img);
@@ -109,15 +114,16 @@ int main(int argc, char** argv) {
     e = ecart_type(mesures, moy);
 	mi = min(mesures);
 	ma = max(mesures);
-    printf("Temps moyen d_image_compute_arena_position:%f (ecart type:%f, min:%f max%f)\n", moy, e, mi, ma);
+	printf(">  Temps moyen: %f ecart type: %f min: %f max: %f\n", moy, e, mi, ma);
 	
 	
 	/*****************************
 	 Temps pour le calcul de la position du robot sans arene
 	 d_image_compute_robot_position
 	 *****************************/
+	printf("Evalution du temps pour d_image_compute_robot_position sans arene\n");
 	arena = NULL;
-    for (i = 0; i < 100; i++) {
+    for (i = 0; i < NBE; i++) {
         if (real) d_camera_get_frame(camera, img);
         t1 = d_tools_get_time_in_millisec();
         d_image_compute_robot_position(img, arena);
@@ -128,12 +134,13 @@ int main(int argc, char** argv) {
     e = ecart_type(mesures, moy);
 	mi = min(mesures);
 	ma = max(mesures);
-    printf("Temps moyen d_image_compute_robot_position sans arene:%f (ecart type:%f, min:%f max%f)\n", moy, e, mi, ma);
+	printf(">  Temps moyen: %f ecart type: %f min: %f max: %f\n", moy, e, mi, ma);
 
 	/*****************************
 	 Temps pour le calcul de la position du robot avec arene
 	 d_image_compute_robot_position
 	 *****************************/
+	printf("Evalution du temps pour d_image_compute_robot_position avec arene\n");
 	if (real) d_camera_get_frame(camera, img);
 	i = 0;
 	while (arena == NULL) {
@@ -142,7 +149,7 @@ int main(int argc, char** argv) {
 		if (i > 10) exit(EXIT_FAILURE);
 	}
 	
-    for (i = 0; i < 100; i++) {
+    for (i = 0; i < NBE; i++) {
         if (real) d_camera_get_frame(camera, img);
         t1 = d_tools_get_time_in_millisec();
         d_image_compute_robot_position(img, arena);
@@ -153,15 +160,16 @@ int main(int argc, char** argv) {
     e = ecart_type(mesures, moy);
 	mi = min(mesures);
 	ma = max(mesures);
-    printf("Temps moyen d_image_compute_robot_position avec arene:%f (ecart type:%f, min:%f max%f)\n", moy, e, mi, ma);
+	printf(">  Temps moyen: %f ecart type: %f min: %f max: %f\n", moy, e, mi, ma);
     
 	
 	/*****************************
 	 Temps pour compresser une image
 	 d_jpegimage_compress
 	 *****************************/
+	printf("Evalution du temps pour d_jpegimage_compress\n");
     DJpegimage* jpeg = d_new_jpegimage();
-    for (i = 0; i < 100; i++) {
+    for (i = 0; i < NBE; i++) {
         if (real) d_camera_get_frame(camera, img);
         t1 = d_tools_get_time_in_millisec();
         d_jpegimage_compress(jpeg, img);
@@ -172,10 +180,8 @@ int main(int argc, char** argv) {
     e = ecart_type(mesures, moy);
 	mi = min(mesures);
 	ma = max(mesures);
-    printf("Temps moyen d_jpegimage_compress:%f (ecart type:%f, min:%f max%f)\n", moy, e, mi, ma);
-	
-    
-	
+	printf(">  Temps moyen: %f ecart type: %f min: %f max: %f\n", moy, e, mi, ma);
+	    
     return 0;
 }
 
